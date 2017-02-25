34f47d449b2b80932352940c8fb0a1e2a7e04a1b
mlefree <mat@mlefree.com>
2017-02-25 22:58:35 +0100

    Refacto done ..

--- www/js/config/config.demo.js
+++ www/js/config/config.demo.js
@@ -8,7 +8,7 @@ if (!configModule) {
 
         // from package.json : extract from gulp config task
         .constant("appName", "cleanator")
-        .constant("appVersion", "17.1.14")
+        .constant("appVersion", "17.1.15")
 
         // from miapp.io
         .constant("miappId", "demo")
